4b222f2f84e68a462d1adb46ec271b253de58eab
xXPhartyGhamerXx <parrygmer@gmail.com>
2019-08-04 20:51:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,54 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js')
+const client = new Discord.Client()
 
-const client = new Discord.Client();
+client.on('message', (receivedMessage) => {
+  client.user.setActivity("Essere un Lorisso Bot")
 
- 
+    if (receivedMessage.author == client.user) { // Prevent bot from responding to its own messages
+        return
+    }
+    
+    if (receivedMessage.content.startsWith("lorisso.bot")) {
+        processCommand(receivedMessage)
+    }
+})
 
-client.on('ready', () => {
+function processCommand(receivedMessage) {
+    let fullCommand = receivedMessage.content.substr(1) // Remove the leading exclamation mark
+    let splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
+    let primaryCommand = splitCommand[0] // The first word directly after the exclamation is the command
+    let arguments = splitCommand.slice(1) // All other words are arguments/parameters/options for the command
 
-    console.log('Pronto!');
+    console.log("Command received: " + primaryCommand)
+    console.log("Arguments: " + arguments) // There may not be any arguments
 
-});
+    if (primaryCommand == "help") {
+        helpCommand(arguments, receivedMessage)
+    } else if (primaryCommand == "multiply") {
+        multiplyCommand(arguments, receivedMessage)
+    } else {
+        receivedMessage.channel.send("Senti, io sono un Lorisso Bot. Non capisco cosa dici. Scusa!")
+    }
+}
 
- 
+function helpCommand(arguments, receivedMessage) {
+    if (arguments.length > 0) {
+        receivedMessage.channel.send("It looks like you might need help with " + arguments)
+    } else {
+        receivedMessage.channel.send("I'm not sure what you need help with. Try `!help [topic]`")
+    }
+}
 
-client.on('message', message => {
+function multiplyCommand(arguments, receivedMessage) {
+    if (arguments.length < 2) {
+        receivedMessage.channel.send("Not enough values to multiply. Try `!multiply 2 4 10` or `!multiply 5.2 7`")
+        return
+    }
+    let product = 1 
+    arguments.forEach((value) => {
+        product = product * parseFloat(value)
+    })
+    receivedMessage.channel.send("The product of " + arguments + " multiplied together is: " + product.toString())
+}
 
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-     
-       }
-
-});
-
- 
-
-// THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);
