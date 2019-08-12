8f9c643c8988ba3eacd3d94380e809f2ac99562b
xXPhartyGhamerXx <parrygmer@gmail.com>
2019-08-12 17:14:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -74,6 +74,11 @@ var memes = Math.floor(Math.random() * 7);
     message.channel.sendMessage('F');
 
        }
+ if (message.content === 'f' || message.content === 'F') {
+    
+    message.channel.sendMessage('F');
+
+       }
 
 
 
